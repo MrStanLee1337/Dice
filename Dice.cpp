deb01615adff093f2bca0f0a8b6e41de78073ec5
MrStanLee1337 <kenmoney@mail.ru>
2025-10-28 21:21:36 +0500

    CMAKELISTS

---
+++ Dice.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <random>
+using namespace std;
+
+mt19937 rnd(static_cast<unsigned int> (time(nullptr)));
+int main() { 
+
+	return 0;
+}
